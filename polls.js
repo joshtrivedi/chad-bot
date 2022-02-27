f0d6bde0e83036508163b4c64493b3ae6e1c40f3
Josh Trivedi <josh.trivedi@gmail.com>
2022-02-27 13:35:44 +0530

    new and improved polls TODO: add functionality to react with nitro emoji

--- polls.js
+++ polls.js
@@ -1,36 +1,52 @@
+const Discord = require('discord.js');
 const emojis = require('./emojis.json')
-
+const regionals = require('./reg-indicator.json')
 const {
     polls_channel
-} = require('./config.json')
+} = require('./config.json'); 
+const something = require('./something');
 
+function checkEmoji(args) {
+    reg = /<.*>$/
+    return reg.test(args)
+}
 
-function isEmojifromJSON(args) {
+function isEmojiAlphabet(args) {
     const matched = 
+    regionals.find(e=>e.char === args)
+    if(matched) return args
+    else return false
+}
+
+function isEmojiUnicode(args) {
+    const matched =
     emojis.find(element => element.char === args)
-    if(matched) return true
+    if(matched) return args
     else return false
 }
 
 module.exports = (bot) => {
-    bot.on('message',msg => {
+    bot.on('message', async msg => {
         const {
             channel, author, content
         } = msg
         
-        if(!polls_channel.find(f=>f.includes(channel.id))) return
-        const backSlash = '\ '.trim()
+        if(!polls_channel.includes(msg.channel.id)) {
+            return
+        }
+        if(msg.author.bot) return
         const eachLine = content.split('\n');
+        console.log(eachLine)
         for (const line of eachLine) {
             const split = line.split(' ')
             var emoji = split[0].trim()
-
-            if(emoji.startsWith(backSlash)) emoji=(emoji.substring(1,emoji.length))
-
-            if(!isEmojifromJSON(emoji)) continue
-            
-            msg.react(emoji)
-
+            //if(!isEmojiUnicode(emoji) && !isEmojiAlphabet(emoji)) break
+            if(isEmojiUnicode(emoji)){
+                msg.react(emoji ?? "")
+            }
+            if(isEmojiAlphabet(emoji)){
+                msg.react(emoji ?? "")
+            }
         }
     })
 }
